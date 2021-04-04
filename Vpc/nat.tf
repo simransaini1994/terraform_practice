4b52f09c5a6fcb17986e19ad17383b8ef211d9f5
Simranjit <simranjeetsingh4444@gmail.com>
2021-04-03 23:30:49 -0400

    adding custom vpc creation terraform

---
+++ Vpc/nat.tf
@@ -0,0 +1,34 @@
+resource "aws_eip" "test-nat" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "test-nat-gw" {
+  allocation_id = aws_eip.test-nat.id
+  subnet_id     = aws_subnet.test-public-1.id
+  depends_on = [aws_internet_gateway.test-gw]
+  tags = {
+    Name = "test-nat-gw"
+  }
+}
+
+resource "aws_route_table" "private-route-table" {
+  vpc_id = aws_vpc.test_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.test-nat-gw.id
+  }
+   tags = {
+    Name = "private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.test-private-1.id
+  route_table_id = aws_route_table.private-route-table.id
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.test-private-2.id
+  route_table_id = aws_route_table.private-route-table.id
+}
